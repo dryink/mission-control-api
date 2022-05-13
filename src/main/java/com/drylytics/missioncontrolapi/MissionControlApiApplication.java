d23c107c19478a343b22abd3d6cfe424dfd006f9
David R Young <dryink@gmail.com>
2022-05-12 21:29:56 -0500

    initial commit

---
+++ src/main/java/com/drylytics/missioncontrolapi/MissionControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.drylytics.missioncontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drylytics/missioncontrolapi/MissionControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drylytics.missioncontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
